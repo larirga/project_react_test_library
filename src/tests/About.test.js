d6b62c230ffc5119e1114d1f1f2b1b400ed2a1fc
Larissa <larisssa_rga@hotmail.com>
2022-11-22 17:03:11 -0300

    Adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex;', () => {
+    renderWithRouter(<About />);
+    const aboutHeading = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+
+    const paragraph1 = screen.getByText(/this application simulates a pokédex, a digital encyclopedia containing all pokémon/i);
+
+    const paragraph2 = screen.getByText(/one can filter pokémon by type, and see more details for each one of them/i);
+
+    const imagePokedex = screen.getByRole('img', { name: /pokédex/i });
+
+    expect(aboutHeading).toBeInTheDocument();
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+    expect(imagePokedex.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
