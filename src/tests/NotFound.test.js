4469128dfe91688bbc2fae9210832636afdd0a07
Larissa <larisssa_rga@hotmail.com>
2022-11-22 18:03:51 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <NotFound.js />', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/notfound'));
+  });
+
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    const errorMessage = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(errorMessage).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem', () => {
+    const imageNotFound = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(imageNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
